91bf8746fb99347a75d60094a30a89de7e830240
Michael Legemah <MichaelLegemah@gmail.com>
2018-08-01 16:59:00 -0400

    Setting up firebase connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCZww2CXkwh3byleSd_SmUkoHJbJqXgVAo',
+    authDomain: 'affluent-cms-database-a6.firebaseapp.com',
+    databaseURL: 'https://affluent-cms-database-a6.firebaseio.com',
+    projectId: 'affluent-cms-database-a6',
+    storageBucket: 'affluent-cms-database-a6.appspot.com',
+    messagingSenderId: '319880768653'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      apiKey: 'AIzaSyCZww2CXkwh3byleSd_SmUkoHJbJqXgVAo',
+      authDomain: 'affluent-cms-database-a6.firebaseapp.com',
+      databaseURL: 'https://affluent-cms-database-a6.firebaseio.com',
+      projectId: 'affluent-cms-database-a6',
+      storageBucket: 'affluent-cms-database-a6.appspot.com',
+      messagingSenderId: '319880768653'
+  }
 };
 
 /*
